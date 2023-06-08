4d92c66ced66c719d8f62abd89bbe650c06c3253
Sankett65 <jadhav.jadhav.sanket@gmail.com>
2023-06-08 19:00:17 +0530

    second commit

--- Basic/src/com/bridgelab/basic/basic.java
+++ Basic/src/com/bridgelab/basic/basic.java
@@ -3,5 +3,6 @@ package com.bridgelab.basic;
 public class basic {
     public static void main(String[] args) {
         System.out.println("Hello Sanket");
+        System.out.println("Hello Swapnil");
     }
 }
